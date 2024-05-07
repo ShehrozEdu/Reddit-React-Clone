e10efddde1a13c501f32c10a3c3b3218200a4c20
Shehroz Batt <101613341+ShehrozEdu@users.noreply.github.com>
2024-05-07 12:43:39 +0530

    workUnderProgress page updated

--- src/components/Home/WorkUnderProgress.jsx
+++ src/components/Home/WorkUnderProgress.jsx
@@ -12,7 +12,7 @@ const WorkUnderProgress = () => {
                         <div class="text-5xl dark:text-white font-dark font-bold">404</div>
                         <p
                             class="text-2xl md:text-3xl dark:text-white font-light leading-normal"
-                        >Sorry we couldn't find this page. </p>
+                        >Work is under progress.</p>
                         <p class="mb-8 dark:text-white">But dont worry, you can find plenty of other things on our homepage.</p>
 
                         <button class="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-gray-600 active:bg-gray-600 hover:bg-gray-700" onClick={()=>navigate("/")}>Back to homepage</button>
